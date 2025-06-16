573ebf3a3ddc85ee26e06b1df09e8361e541d514
Alghisi Alessandro Paolo <alexalghisi@yahoo.com>
2025-06-16 12:17:48 +0300

    Cleaning code & nit picks

--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,3 @@
-export { mockRecipeAPI } from './mockRecipeAPI';
 export { openaiRecipeAPI } from './openaiRecipeAPI';
 export { recipeAPI } from './recipeAPI';
+export { mockRecipeAPI } from './mockRecipeAPI';
